750eb045d8ab877a75c957db8d2ac03e223e6df7
Gabrielle-Tobermann <gabrielle.tobermann@gmail.com>
2021-05-04 20:02:09 -0500

    setup auth, and navBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavbarText,
+  Button
+} from 'reactstrap';
+import { signInUser, signOutUser } from '../helpers/auth';
+
+const NavBar = ({ user }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  const authenticated = () => (
+    <>
+    <NavItem>
+      <Link className="nav-link" to="/add-player">Add Students</Link>
+    </NavItem>
+    <NavItem>
+      <Link className="nav-link" to="/">Team</Link>
+    </NavItem>
+    </>
+  );
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+         {user && authenticated()}
+         <div>
+           {
+             user !== null
+             && <NavItem>
+               {
+                  user
+                    ? <Button color="info" onClick={signOutUser}>Sign Out</Button>
+                    : <Button color="info" onClick={signInUser}>Sign In</Button>
+               }
+             </NavItem>
+           }
+         </div>
+          </Nav>
+          <NavbarText>Simple Text</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+NavBar.propTypes = {
+  user: PropTypes.any
+};
+
+export default NavBar;
